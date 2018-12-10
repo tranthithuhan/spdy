f2e72e3fac052cff4e9d1e8b023d182e9288f42e
Hân <tranthithuhan@gmail.com>
2018-12-10 21:36:44 +0100

    test send kafka event

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.status(200).send("ok");
-});
+const {HighLevelProducer, KafkaClient} = require('kafka-node');
+
+let kafkaUri = process.env["KAFKA_URI"] ? process.env["KAFKA_URI"] : 'kafka.kafka:9092';
 
 let messageId = 0;
 
-router.get('/pushy', (req, res) => {
+router.get("/", (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
+this.producer = new HighLevelProducer(new KafkaClient({kafkaHost: kafkaUri}));
+this.producer.on('ready', () => {
+    console.log("Connected successfully to Kafka server");
+});
 
-    setInterval(function(){
+let message = JSON.stringify({
+    type: "USER_STATUS",
+    payload: {
+        status: "ONLINE"
+    }
+});
+
+this.producer.send([{
+    topic: "server.events",
+    messages: message
+}], (err) => err !== null ? "error" : console.log("send event", message))
+
+setInterval(function(){
     var msg = JSON.stringify({'msg': messageId});
     var resourcePath = '/resource/'+messageId;
     res.push(resourcePath, {}, function(err, stream) { stream.end(msg) });
@@ -20,22 +36,22 @@ router.get('/pushy', (req, res) => {
     messageId+=1;
 }, 2000);
 
-    // var stream = res.push('/main.js', {
-    //     status: 200,
-    //     method: 'GET',
-    //     request: {
-    //         accept: '*/*'
-    //     }
-    // });
-    //
-    // stream.on('error', function () {
-    //
-    // });
-    //
-    // stream.end('Hello');
-    //
-    // res.end('<script src="/main.js">aaa</script>');
-    // res.end('Hello World! <script src="/main.js"></script>');
+// var stream = res.push('/main.js', {
+//     status: 200,
+//     method: 'GET',
+//     request: {
+//         accept: '*/*'
+//     }
+// });
+//
+// stream.on('error', function () {
+//
+// });
+//
+// stream.end('Hello');
+//
+// res.end('<script src="/main.js">aaa</script>');
+// res.end('Hello World! <script src="/main.js"></script>');
 });
 
 module.exports = router;
